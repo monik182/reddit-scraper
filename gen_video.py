dd1b4edb2f9936374751faa27f30fb71061c856d
Monica Carvajal <monik.182@gmail.com>
2024-11-08 01:23:49 +0100

    update new audio code

--- gen_video.py
+++ gen_video.py
@@ -1,5 +1,6 @@
 import moviepy.editor as mp
 from pathlib import Path
+import moviepy.audio.fx.all as afx
 
 def replace_audio(video_path, audio_path, output_path):
     video_path = str(video_path)
@@ -11,7 +12,11 @@ def replace_audio(video_path, audio_path, output_path):
     
     video = video.set_audio(new_audio).set_duration(new_audio.duration)
 
-    new_audio = new_audio.volumex(1.5)
+    # new_audio = new_audio.volumex(1.5)
+    new_audio = mp.AudioFileClip(audio_path)
+    
+    # Normalize the audio volume to ensure it is audible
+    new_audio = afx.audio_normalize(new_audio)
     video = video.set_audio(new_audio)
     
     video.write_videofile(output_path, codec='libx264', audio_codec='aac')
