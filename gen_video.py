694d739ef3742777113b091ab8ed7106b444f128
Monica Carvajal <monik.182@gmail.com>
2024-11-08 01:01:25 +0100

    - update gitigonre - create video generator script

---
+++ gen_video.py
@@ -0,0 +1,27 @@
+import moviepy.editor as mp
+from pathlib import Path
+
+def replace_audio(video_path, audio_path, output_path):
+    video_path = str(video_path)
+    audio_path = str(audio_path)
+    output_path = str(output_path)
+
+    video = mp.VideoFileClip(video_path)
+    new_audio = mp.AudioFileClip(audio_path)
+    
+    video = video.set_audio(new_audio).set_duration(new_audio.duration)
+
+    new_audio = new_audio.volumex(1.5)
+    video = video.set_audio(new_audio)
+    
+    video.write_videofile(output_path, codec='libx264', audio_codec='aac')
+
+def main():
+    video_file = Path("output_videos/video1.mp4")
+    english_audio_file = Path("en.mp3")
+    output_video_file = Path("output_video_with_new_audio.mp4")
+    
+    replace_audio(video_file, english_audio_file, output_video_file)
+
+if __name__ == "__main__":
+    main()
